960cfdaa1fe4883a1c0a7845692136afb3361793
alexzemz <zemzaleks@gmail.com>
2021-12-12 16:09:04 +0000

    Header Complited

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>Web development and website promotions</h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            "Web Design",
+            "Development",
+            "Facebook Ads SMM",
+            "Google Ads",
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
