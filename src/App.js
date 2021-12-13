09370fc95a56aa6a8c5a6d27a4621c70b9339623
alexzemz <zemzaleks@gmail.com>
2021-12-13 13:20:00 +0000

    Fixed Particles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Particles from "react-particles-js";
+// import Particles from "react-particles-js";
+import Particles from "react-tsparticles";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
@@ -8,24 +9,101 @@ function App() {
   return (
     <>
       <Particles
-        className="particles-canvas"
-        params={{
+        className="tsparticles-canvas-el"
+        // params={{
+        //   particles: {
+        //     number: {
+        //       value: 30,
+        //       density: {
+        //         enable: true,
+        //         value_area: 900,
+        //       },
+        //     },
+        //     shape: {
+        //       type: "circle",
+        //       stroke: {
+        //         width: 6,
+        //         color: "#f9ab00",
+        //       },
+        //     },
+        //   },
+        // }}
+        options={{
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
           particles: {
+            color: {
+              value: "#f9ab00",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 1,
+              straight: false,
+            },
             number: {
-              value: 30,
               density: {
                 enable: true,
                 value_area: 900,
               },
+              value: 50,
+            },
+            opacity: {
+              value: 0.5,
             },
             shape: {
               type: "circle",
-              stroke: {
-                width: 6,
-                color: "#f9ab00",
-              },
+            },
+            size: {
+              random: true,
+              value: 5,
             },
           },
+          detectRetina: true,
         }}
       />
       <Navbar />
